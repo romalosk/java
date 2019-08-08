8ea6955eea24dbc5afeb114c5b0e1c674f7533b8
romalosk <tihon.exp@gmail.com>
2019-08-09 00:50:29 +0400

    HW

---
+++ hw2.js
@@ -0,0 +1,10 @@
+var number1 = prompt("Введите первое число");
+var number2 = prompt("Введите второе число");
+if (number1 > number2) {
+  alert("Число " + number1 + " больше");
+} else if (number1 < number2) {
+  alert("Число " + number2 + " больше");
+} else { (number1 == number2); {
+  alert("Числа равны");
+  }
+  }
---
+++ hw3.js
@@ -0,0 +1,25 @@
+var user = {
+    name:prompt ('Введите ваше имя'),
+    age:prompt ('Введите ваш возраст')
+  }
+  console.log(user);
+
+  ///
+
+var arr = [];
+arr[0] = prompt();
+arr[1] = prompt();
+arr[2] = prompt();
+arr[3] = prompt();
+arr[4] = prompt();
+var i = 4;
+while (i>=0) {
+    console.log(arr[i]);
+i--;
+}
+
+///
+
+for (i=4; i>=0; i--) {
+    console.log(arr[i]);
+}
