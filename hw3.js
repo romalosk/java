a4c4f4fe12b31c5d112959f41947dfc460506e67
romalosk <tihon.exp@gmail.com>
2019-08-11 23:29:14 +0400

    В процессе

--- hw3.js
+++ hw3.js
@@ -6,20 +6,20 @@ var user = {
 
   ///
 
-var arr = [];
-arr[0] = prompt();
-arr[1] = prompt();
-arr[2] = prompt();
-arr[3] = prompt();
-arr[4] = prompt();
+var myArr = [];
+myArr[0] = prompt();
+myArr[1] = prompt();
+myArr[2] = prompt();
+myArr[3] = prompt();
+myArr[4] = prompt();
 var i = 4;
 while (i>=0) {
-    console.log(arr[i]);
+    console.log(myArr[i]);
 i--;
 }
 
 ///
 
 for (i=4; i>=0; i--) {
-    console.log(arr[i]);
+    console.log(myArr[i]);
 }
---
+++ hw4.js
@@ -0,0 +1,22 @@
+var myArr = [];
+myArr[0] = prompt();
+myArr[1] = prompt();
+myArr[2] = prompt();
+myArr[3] = prompt();
+myArr[4] = prompt();
+myArr.reverse();
+console.log(myArr);
+
+///
+
+var str = "we have a text. with a small words. after space";
+str.split('. ');
+function texttransform() {
+  return str.charAt(0).toUpperCase;
+}
+console.log(texttransform);
+
+///
+
+var str = "В лесу была хорошая погода. Кукушка куковала.";
+console.log(str.substr(0, 26));
