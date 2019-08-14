c7bc8448b9d96662cb7b9ae03f5b3b17fddfc286
romalosk <tihon.exp@gmail.com>
2019-08-14 15:55:18 +0400

    Excelent

--- hw5.js
+++ hw5.js
@@ -2,3 +2,12 @@ var div = document.createElement('div');
 div.className = "block";
 div.innerHTML = "JavaScript";
 document.body.append(div);
+
+///
+
+console.log(document.getElementsByClassName("main")[0].innerHTML);
+
+///
+
+document.body.classList.add('discription', 'image');
+console.log(document.body.className);
