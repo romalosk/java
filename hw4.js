ca1374c643028cd0f27940444390f28a484bc605
romalosk <tihon.exp@gmail.com>
2019-08-16 00:11:21 +0400

    Пятая готова

--- hw4.js
+++ hw4.js
@@ -1,20 +1,20 @@
 var myArr = [];
-myArr[0] = prompt();
-myArr[1] = prompt();
-myArr[2] = prompt();
-myArr[3] = prompt();
-myArr[4] = prompt();
+myArr[0] = prompt('Введите число');
+myArr[1] = prompt('Введите число');
+myArr[2] = prompt('Введите число');
+myArr[3] = prompt('Введите число');
+myArr[4] = prompt('Введите число');
 myArr.reverse();
 console.log(myArr);
 
 ///
 
-var str = "we have a text. with a small words. after space";
-str.split('. ');
-function texttransform() {
-  return str.charAt(0).toUpperCase;
-}
-console.log(texttransform);
+// var str = "we have a text. with a small words. after space";
+// str.split('. ');
+// function texttransform() {
+//   return str.charAt(0).toUpperCase;
+// }
+// console.log(texttransform);
 
 ///
 
