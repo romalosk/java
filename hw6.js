ceaf8fc15f643069ce671fe012bf7b1fc7a9ab31
romalosk <tihon.exp@gmail.com>
2019-08-23 00:58:06 +0400

    сделал шестое

--- hw6.js
+++ hw6.js
@@ -1,6 +1,20 @@
-var block = document.getElementsByClassName('button');
-block.onclick = function changerstyle () {
-    block.style.backgroundColor = "green";
-};
+// var block = document.getElementsByClassName("button-red")[0];
+// block.onclick = function changerstyle () {
+//     block.style.backgroundColor = "green";
+//     block.style.color = "white";
+// }
 
-block.addEventListener("click", changerstyle);
+// block.addEventListener("click", changerstyle);
+
+///
+
+var newblock = document.getElementsByClassName("button-green")[0];
+newblock.onclick = function createblock () {
+    let div = document.createElement('div');
+    div.className = "button-blue";
+    div.innerHTML = "Сюрприз!";
+    document.body.append(div);
+    newblock.removeEventListener("click", createblock);
+}
+
+newblock.addEventListener("click", createblock);
